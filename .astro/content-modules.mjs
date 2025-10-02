15af13b64a906d10acebec0502974920f14d5701
phoosy <phoosy@phoosy.com>
2025-10-02 12:35:40 -0500

    more blog progress hee hee

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,5 +1,6 @@
 
 export default new Map([
 ["src/content/blog/00-first-entry/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F00-first-entry%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/blog/01-maresync-saga/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F01-maresync-saga%2Findex.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/01-maresync-saga/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F01-maresync-saga%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/blog/02-moving-to-cachyos/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F02-moving-to-cachyos%2Findex.mdx&astroContentModuleFlag=true")]]);
 		
