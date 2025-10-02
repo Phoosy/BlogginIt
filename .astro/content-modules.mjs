11d89ef364d9d532eefecd07879415b7486b8005
phoosy <phoosy@phoosy.com>
2025-10-02 01:13:31 -0500

    formatting tweaks to saga blog

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,5 @@
 
 export default new Map([
-["src/content/blog/00-first-entry/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F00-first-entry%2Findex.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/00-first-entry/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F00-first-entry%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/blog/01-maresync-saga/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2F01-maresync-saga%2Findex.mdx&astroContentModuleFlag=true")]]);
 		
