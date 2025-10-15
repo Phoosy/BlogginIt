aebaf56a0c1923ff75cf7c15aca6f344a2f0742d
phoosy <phoosy@phoosy.com>
2025-10-14 23:16:28 -0500

    swap to noto from geist, was breaking in vivaldi for some reason...

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -2,6 +2,6 @@
 export default new Map([
 ["src/content/blog/first-entry/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Ffirst-entry%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/maresync-saga/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmaresync-saga%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/blog/yubikey-adventures/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fyubikey-adventures%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/blog/moving-to-cachyos/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmoving-to-cachyos%2Findex.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/moving-to-cachyos/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmoving-to-cachyos%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/blog/yubikey-adventures/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fyubikey-adventures%2Findex.mdx&astroContentModuleFlag=true")]]);
 		
